8ee455844b9c783390c25e1442adf0ca0a60011d
Mustafa <cobanium@icloud.com>
2022-05-13 16:17:21 +0300

    Add navigation component to main screen

--- Pokedex/PokemonSearchViewController.swift
+++ Pokedex/PokemonSearchViewController.swift
@@ -11,5 +11,18 @@ class PokemonSearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        title = Constants.NavigationBar.navigationTitle
+    }
+}
+
+// MARK: - Constants
+extension PokemonSearchViewController {
+    
+    /// contains the constant values used in the view controller
+    enum Constants {
+        enum NavigationBar {
+            static let navigationTitle = "Pokedex"
+        }
     }
 }
